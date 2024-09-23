02293e60ec3621ff845037c2bd026562884494fa
marcusheisters <marcus.heisters@googlemail.com>
2024-09-23 17:16:31 +0300

    Finish Converter

--- decimal-to-hex-converter/src/main.c
+++ decimal-to-hex-converter/src/main.c
@@ -6,6 +6,12 @@ int main() {
 	printf("Welcome to Decimal to Hex Converter\n");
 	printf("Enter a number:\n");
 
-	int hexNumber;
-	scanf("%d", &hexNumber);
+	int number;
+	scanf("%d", &number);
+
+	printf("Decimal representation: %9d\n", number);
+	printf("Converted to hexadecimal: %9X\n", number);
+	printf("===^^===");
+
+	return 0;
 }
