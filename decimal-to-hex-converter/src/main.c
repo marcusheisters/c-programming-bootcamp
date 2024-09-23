c52ac36270c0bcb17198aa300cf9c3b34af4d039
marcusheisters <marcus.heisters@googlemail.com>
2024-09-23 17:08:24 +0300

    First programm code

---
+++ decimal-to-hex-converter/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("===^^===");
+	printf("Welcome to Decimal to Hex Converter\n");
+	printf("Enter a number:\n");
+
+	int hexNumber;
+	scanf("%d", &hexNumber);
+}
